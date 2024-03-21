f359e6f7b565fe29560f515a69286fe11de2d748
kkumaki12 <k.kumaki12@gmail.com>
2024-03-21 21:46:26 +0900

    index.tsx

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -20,4 +20,18 @@ app.get('/posts/:id', (c) => {
   return c.text(`You want see ${page} of ${id}`)
 })
 
+const View = () => {
+  return (
+    <html>
+      <body>
+        <h1>Hello Hono!</h1>
+      </body>
+    </html>
+  )
+}
+
+app.get('/page', (c) => {
+  return c.html(<View />)
+})
+
 export default app
